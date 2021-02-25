758c44a2d95ceb010ad3fd482652ae6f05f0a7f7
mateus.cantanhede <mateus.cantanhede@SUINF-106785.ssp.ma.gov.br>
2021-02-25 13:18:06 -0300

    edição do data.sql, e modelo

--- src/main/java/com/example/demo/modelo/Resposta.java
+++ src/main/java/com/example/demo/modelo/Resposta.java
@@ -2,15 +2,22 @@ package com.example.demo.modelo;
 
 import java.time.LocalDateTime;
 
-import org.springframework.boot.autoconfigure.domain.EntityScan;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
-@EntityScan
+@Entity
 public class Resposta {
 
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String mensagem;
+	@ManyToOne
 	private Topico topico;
 	private LocalDateTime dataCriacao;
+	@ManyToOne
 	private Usuario autor;
 	private Boolean solucao = false;
 
---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno','aluno@email.com','123456');
+
+INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot','Programação');
+INSERT INTO CURSO(nome, categoria) VALUES('HTML5','Front-End');
+
+INSERT INTO TOPICO(titulo, mensagem, data_criacao, status, autor_id, curso_id) VALUES('Duvida','Erro ao criar projeto','2019-05-10 );
+INSERT INTO TOPICO(titulo, mensagem, data_criacao, status, autor_id, curso_id) VALUES('Duvida2',);
+INSERT INTO TOPICO(titulo, mensagem, data_criacao, status, autor_id, curso_id) VALUES('Duvida3',);
