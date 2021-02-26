216a79a489d2b3d0a1a7f42823823e4b747e22f9
Mateus de Jesus Rocha Cantanhede <mateus.cantanhede@ssp.ma.gov.br>
2021-02-26 13:02:34 -0300

    add confg validation e repository

--- src/main/java/com/example/demo/modelo/Topico.java
+++ src/main/java/com/example/demo/modelo/Topico.java
@@ -31,10 +31,10 @@ public class Topico {
 	private Curso curso;
 	@OneToMany(mappedBy = "topico")
 	private List<Resposta> respostas = new ArrayList<>();
-
+	
 	public Topico() {
 	}
-
+	
 	public Topico(String titulo, String mensagem, Curso curso) {
 		this.titulo = titulo;
 		this.mensagem = mensagem;
