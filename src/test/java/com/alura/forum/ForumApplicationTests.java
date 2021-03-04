ab35d591c2da2b7e949bb1e4ab4c5ff31da7c8e1
mateus.cantanhede <mateus.cantanhede@SUINF-106785.ssp.ma.gov.br>
2021-03-04 08:31:26 -0300

    add actuator module

--- src/test/java/com/alura/forum/ForumApplicationTests.java
+++ src/test/java/com/alura/forum/ForumApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.alura.forum;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class ForumApplicationTests {
 
 	@Test
 	void contextLoads() {
